519ed77b00acbb1540c98640059b285de9f0a6d8
key-amb <yasutake.kiyoshi@gmail.com>
2016-10-08 16:02:29 +0900

    Modify / Use inherited G::Executable#rsync

--- lib/grifork/executor/grifork.rb
+++ lib/grifork/executor/grifork.rb
@@ -8,14 +8,21 @@ class Grifork::Executor::Grifork
   def run(node)
     c = config.grifork
     ssh node.host, %(test -d "#{c.workdir}" || mkdir -p "#{c.workdir}"), user: c.login
-    sh :rsync, ['-avzc', config.griforkfile, "#{node.host}:#{c.workdir}/Griforkfile"]
+    rsync(node.host, config.griforkfile, "#{c.workdir}/Griforkfile")
+    prepare_grifork_hosts_file_on_remote(node)
+    ssh node.host, %(cd #{c.dir} && #{c.cmd} --file #{c.workdir}/Griforkfile --override-by #{c.workdir}/Griforkfile.hosts --on-remote), [], user: c.login
+  end
+
+  private
+
+  def prepare_grifork_hosts_file_on_remote(node)
+    c = config.grifork
     hostsfile = Tempfile.create('Griforkfile.hosts')
     hostsfile.write(<<-EOS)
       hosts #{node.all_descendant_nodes.map(&:host)}
     EOS
     hostsfile.flush
-    sh :rsync, ['-avzc', hostsfile.path, "#{node.host}:#{c.workdir}/Griforkfile.hosts"]
+    rsync(node.host, hostsfile.path, "#{c.workdir}/Griforkfile.hosts")
     hostsfile.close
-    ssh node.host, %(cd #{c.dir} && #{c.cmd} --file #{c.workdir}/Griforkfile --override-by #{c.workdir}/Griforkfile.hosts --on-remote), [], user: c.login
   end
 end
